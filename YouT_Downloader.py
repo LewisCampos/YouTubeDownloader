0129b6c72a79aa7cc6809d6e3224fb74b4c936a7
Lewcamps <lewcamps53@gmail.com>
2021-06-22 16:15:51 -0430

    Descargar VIIII

--- YouT_Downloader.py
+++ YouT_Downloader.py
@@ -3,10 +3,24 @@ from pytube import YouTube
 from tkinter import messagebox, filedialog
 
 ws=Tk()
-ws.geometry("600x120")
+ws.geometry("470x110")
 ws.resizable(False,False)
 ws.title("Descarga$Video$YouTube")
 ws.config(bg="#940404")
+
+#Logica
+def Buscar():
+    directorio_descargar= filedialog.askdirectory(initialdir="RUTA DE DIRECTORIO")
+    ruta_descarga.set(directorio_descargar)
+
+def Descargar():
+    link_Youtube= link_video_ingresado.get()
+    carpeta_descarga= ruta_descarga.get()
+    toma_video= YouTube(link_Youtube)
+    stream_video=toma_video.streams.first()
+    stream_video.download(carpeta_descarga)
+
+    messagebox.showinfo("COMPLETADO", f"DESCARGADO Y GUARDADO EN {carpeta_descarga}")
 
 
 #Pantalla:
@@ -24,24 +38,10 @@ ruta_descarga=StringVar()
 entry_destino=Entry(ws, width=40, textvariable=ruta_descarga)
 entry_destino.grid(row=2, column=1, padx=5, pady=5)
 
-boton_busqueda= Button (ws, text="Buscar", width=10, bg="#D8D8D8")
+boton_busqueda= Button (ws, text="Buscar", width=10, bg="#D8D8D8", command=Buscar)
 boton_busqueda.grid(row=2, column=2, pady=1, padx=1)
 
-boton_descargar= Button(ws, text="Descargar", width=20, bg="#D8D8D8")
+boton_descargar= Button(ws, text="Descargar", width=20, bg="#D8D8D8", command=Descargar)
 boton_descargar.grid(row=3, column=1, padx=3, pady=3)
-
-def Buscar():
-    directorio_descargar= filedialog.askdirectory(initialdir="RUTA DE DIRECTORIO")
-    ruta_descarga.set(directorio_descargar)
-
-def Descargar():
-    link_Youtube= link_video_ingresado.get()
-    carpeta_descarga= ruta_descarga.get()
-    toma_video= YouTube(link_Youtube)
-    stream_video=toma_video.streams.first()
-    stream_video.download(carpeta_descarga)
-
-    messagebox.showinfo("COMPLETADO", f"DESCARGADO Y GUARDADO EN {carpeta_descarga}")
-
 
 ws.mainloop()
